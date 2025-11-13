52a98f51825d94c7ffcfeda541aae2c5cee610b8
TrungHieu0510 <nthieu2415@clc.fitus.edu.vn>
2025-11-13 14:09:17 +0700

    detail

--- routes/food/__init__.py
+++ routes/food/__init__.py
@@ -2,4 +2,6 @@ from flask import Blueprint
 food_bp = Blueprint('food', __name__)
 
 # Import các route để đăng ký vào blueprint (KHÔNG import ngược lại food_bp)
-from . import search_route, list_all_foods_route
+from . import search_route
+from . import list_all_foods_route
+from . import detail_route
